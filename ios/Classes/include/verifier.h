35cd2089131300c14766d0b044fb1703659b84f9
szelemeh <subzarro6@gmail.com>
2021-06-01 12:50:17 +0200

    Filter erroreous frames

---
+++ ios/Classes/include/verifier.h
@@ -0,0 +1,8 @@
+//
+// Created by Stas on 31/05/2021.
+//
+
+#ifndef PPG_CPP_APP_VERIFIER_H
+#define PPG_CPP_APP_VERIFIER_H
+
+#endif //PPG_CPP_APP_VERIFIER_H
---
+++ ios/Classes/src/verifier.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Stas on 31/05/2021.
+//
+
